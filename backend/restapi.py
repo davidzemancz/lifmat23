b287f4f6f38ef3426be0ab0d445ec5c526df4ccd
David Zeman <david.zeman@outlook.com>
2023-10-24 15:18:39 +0200

    post

--- backend/restapi.py
+++ backend/restapi.py
@@ -3,25 +3,20 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+messages = []
 
 @app.route('/messages')
 def get_messages():
+    global messages
+
     return {
-        'messages': [
-            {
-                'id': 1,
-                'isOutgoing': True,
-                'text': 'Kolik paralenu si muzu dat'
-            },
-            {
-                'id': 2,
-                'isOutgoing': False,
-                'text': 'Zadny ty magore'
-            }
-        ]
+        'messages': messages
     }
 
 @app.route('/post-message', methods=['POST'])
 def post_message():
-    data = request.json
-    return data
+    global messages
+    
+    message = request.json
+    messages.append(message)
+    return {}
