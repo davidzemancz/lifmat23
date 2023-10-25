c310ce4b9bfca73d5ba385d6706d9c2b26be9a23
kwimbik <kwimbiks@gmail.com>
2023-10-25 02:59:00 +0200

    debug pdf reader

--- backend/PDFreader.py
+++ backend/PDFreader.py
@@ -6,6 +6,7 @@ def read_chapter(chap_num, pdf_names):
     #print(pdf_names)
     if len(pdf_names) > 2:
          return "Prosim o specifikaci"
+    
     chap_content = ""
     for p in pdf_names:
         pdf_path = f'../../lifmat23_data/spc/{p}'
@@ -18,6 +19,7 @@ def read_chapter(chap_num, pdf_names):
 
         for i in text.splitlines():
             line = i.strip()
+            print(line.replace(" ", ""))
             if kapitoly[chap_num] in line.replace(" ", ""):
                 read = True
             if len(kapitoly) > chap_num + 1 and kapitoly[chap_num + 1] in line.replace(" ", "") and read == True:
@@ -32,21 +34,25 @@ kapitoly = [
     "1.NÁZEVPŘÍPRAVKU",                                                             #0
     "2.KVALITATIVNÍAKVANTITATIVNÍSLOŽENÍ",                                          #1
     "3.LÉKOVÁFORMA",                                                                #2                                         
-    "4.1Terapeutickéindikace",
-    "4.2Dávkováníazpůsobpodání",
-    "4.3Kontraindikace",
-    "4.4Zvláštníupozorněníaopatřenípropoužití",
-    "4.5Interakcesjinýmiléčivýmipřípravkyajinéformyinterakce",
-    "4.6Fertilita,těhotenstvíakojení",
-    "4.7Účinkynaschopnostříditaobsluhovatstroje",
-    "4.8Nežádoucíúčinky",
-    "4.9Předávkování",
-    "5.1Farmakodynamickévlastnosti",
-    "5.2Farmakokinetickévlastnosti",
-    "5.3Předklinickéúdajevztahujícísekbezpečnosti",
+    "4.1.Terapeutickéindikace",
+    "4.2.Dávkováníazpůsobpodání",
+    "4.3.Kontraindikace",
+    "4.4.Zvláštníupozorněníaopatřenípropoužití",
+    "4.5.Interakcesjinýmiléčivýmipřípravkyajinéformyinterakce",
+    "4.6.Fertilita,těhotenstvíakojení",
+    "4.7.Účinkynaschopnostříditaobsluhovatstroje",
+    "4.8.Nežádoucíúčinky",
+    "4.9.Předávkování",
+    "5.1.Farmakodynamickévlastnosti",
+    "5.2.Farmakokinetickévlastnosti",
+    "5.3.Předklinickéúdajevztahujícísekbezpečnosti",
     "6.FARMACEUTICKÉÚDAJE",
     "7.DRŽITELROZHODNUTÍOREGISTRACI",
     "8.REGISTRAČNÍČÍSLO(A)",
     "9.DATUMPRVNÍREGISTRACE/PRODLOUŽENÍREGISTRACE",
     "10.DATUMREVIZETEXTU"
 ]
+
+pdf = "SPC198710.pdf"
+print(read_chapter(4,[pdf]))
+
